d10dc18ea26c107366acd9158dec366aaa17da5b
LonelyGlare <118815680+LonelyGlare@users.noreply.github.com>
2023-01-08 20:45:30 +0100

    Version final con fallo de logs

--- scpVM1.sh
+++ scpVM1.sh
@@ -1,22 +1,28 @@
-       
+##
+if ! grep -q "${mountdir}.*ext4" /etc/fstab; then
+cat << _EOF_ >> /etc/fstab
+# ${lvname} volume
+${mapper} ${mountdir} ext4 defaults,auto,nofail 0 0
+_EOF_
+fi      
         
         
-sudo apt-get update >/dev/null 2>&1
-sudo apt install -y nginx mariadb-server mariadb-common php-fpm php-mysql expect 
-sudo apt install -y php-curl php-gd php-intl php-mbstring php-soap
-sudo apt install -y php-xml php-xmlrpc php-zip 
+ apt-get update >/dev/null 2>&1
+ apt install -y mariadb-server mariadb-common nginx php-fpm php-mysql expect 
+ apt install -y php-curl php-gd php-intl php-mbstring php-soap
+ apt install -y php-xml php-xmlrpc php-zip 
 
 ##Puntos montaje 
-sudo parted /dev/sdc -s mklabel gpt
-##sudo wipefs -a /dev/sdc ##Borrar tabla de particiones antigua para crear volumen fisico ##Debido a no usar vagrant destroy
-sudo pvcreate /dev/sdc
-sudo vgcreate BDD /dev/sdc
-sudo lvcreate -n MariaBDD -l 100%FREE BDD
-sudo mkfs.ext4 /dev/BDD/MariaBDD
-sudo mount /dev/BDD/MariaBDD /var/lib/mysql ##Espacio entre rutas. Montaje necesita archivo de destino
+ parted /dev/sdc -s mklabel gpt
+ wipefs -a /dev/sdc ##Borrar tabla de particiones antigua para crear volumen fisico ##Debido a no usar vagrant destroy
+ pvcreate /dev/sdc
+ vgcreate BDD /dev/sdc
+ lvcreate -n MariaBDD -l 100%FREE BDD
+ mkfs.ext4 /dev/BDD/MariaBDD
+ mount /dev/BDD/MariaBDD /var/lib/mysql ##Espacio entre rutas. Montaje necesita archivo de destino
 
 ##Configuracion Wordpress Nginx
-cat << EOF | sudo tee /etc/nginx/sites-available/wordpress.conf
+cat << EOF |  tee /etc/nginx/sites-available/wordpress.conf
     server {
         listen 80;
         root /var/www/wordpress;
@@ -51,15 +57,21 @@ sed -i 's/username_here/wpuser/g' /var/www/wordpress/wp-config.php
 sed -i 's/password_here/keepcodingWP/g' /var/www/wordpress/wp-config.php
 
 ##Configure nginx
-cp /vagrant/software/nginx.conf /etc/nginx/sites-available/wordpress
+cp /vagrant/software/nginx-wp.conf /etc/nginx/sites-available/wordpress
 rm -f /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/default
 systemctl restart nginx #Reincio necesario para actualizar nginx
 
 ##Install filebeat
-apt-get install -y filebeat
+##apt-get install apt-transport-https
+##wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+##echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.5.3-amd64.deb
+sudo dpkg -i filebeat-8.5.3-amd64.deb
+##apt-get install -y filebeat
+systemctl restart filebeat
 filebeat modules enable system
 filebeat modules enable nginx
 rm -f /etc/filebeat/filebeat.yml
 cp /vagrant/software/filebeat.yml /etc/filebeat/filebeat.yml
-systemctl enable filebeat --now
+##systemctl enable filebeat --now
--- scpVM2.sh
+++ scpVM2.sh
@@ -1,34 +1,59 @@
-sudo apt update >/dev/null 2>&1
-sudo apt install -y nginx mariadb-server mariadb-common php-fpm php-mysql expect 
-sudo apt install -y php-curl php-gd php-intl php-mbstring php-soap
-sudo apt install -y php-xml php-xmlrpc php-zip 
-sudo parted /dev/sdc -s mklabel gpt
-sudo wipefs -a /dev/sdc ##Borrar tabla de particiones antigua para crear volumen fisico
-sudo pvcreate /dev/sdc
-sudo vgcreate BDD /dev/sdc
-sudo lvcreate -n MariaBDD -l 100%FREE BDD
-sudo mkfs.ext4 /dev/BDD/MariaBDD
-sudo mount /dev/BDD/MariaBDD /var/lib/mysql ##Espacio entre rutas. Montaje necesita archivo de destino
-cat << EOF | sudo tee /etc/nginx/sites-available/wordpress.conf
-    server {
-        listen 80;
-        root /var/www/wordpress;
-        index index.php index.html index.htm index.nginx-debian.html;
-        server_name localhost;
-        location / {
-            try_files \$uri \$uri/ =404;
-        }
-        location ~ \.php\$ {
-            include snippets/fastcgi-php.conf;
-            fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
-        }
-        location ~ /\.ht {
-            deny all;
-        }
-    }
-EOF
-sudo rm /etc/nginx/sites-available/default
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-enabled/wordpress.conf   
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password derp" # new password for the MySQL root user
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password derp" # repeat password for the MySQL root user
+##
+if ! grep -q "${mountdir}.*ext4" /etc/fstab; then
+cat << _EOF_ >> /etc/fstab
+# ${lvname} volume
+${mapper} ${mountdir} ext4 defaults,auto,nofail 0 0
+_EOF_
+fi
+
+apt-get update -y >/dev/null 2>&1
+apt install -y nginx php-fpm php-mysql expect 
+apt install -y php-curl php-gd php-intl php-mbstring php-soap
+apt install -y php-xml php-xmlrpc php-zip 
+
+##Puntos montaje 
+parted /dev/sdc -s mklabel gpt
+wipefs -a /dev/sdc ##Borrar tabla de particiones antigua para crear volumen fisico ##Debido a no usar vagrant destroy
+pvcreate /dev/sdc
+vgcreate BDD /dev/sdc
+lvcreate -n ElasticSearch -l 100%FREE BDD
+mkfs.ext4 /dev/BDD/ElasticSearch
+mount /dev/BDD/ElasticSearch /var/lib/elasticsearch ##Espacio entre rutas. Montaje necesita archivo de destino (pointer)
+
+##Obtain ElasticSearch https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
+ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+    apt-get install apt-transport-https
+    echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
+    apt-get update -y
+
+
+##Install JRE
+apt-get install -y default-jre
+
+##Install ElasticSearch
+apt-get install -y elasticsearch
+chown elasticsearch:elasticsearch /var/lib/elasticsearch
+systemctl daemon-reload ##Necesario para reiniciar lo que el sistema ha leido de los unit files
+systemctl enable elasticsearch --now
+
+
+##Obtain & install Logstash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get install -y logstash
+cp /vagrant/software/02-beats-input.conf /etc/logstash/conf.d/
+cp /vagrant/software/10-syslog.filter.conf /etc/logstash/conf.d/
+cp /vagrant/software/30.elasticsearch-output.conf /etc/logstash/conf.d/
+chmod 644 /etc/logstash/conf.d/*.conf
+systemctl enable logstash --now
+
+
+
+#Install Kibana & authn Kibana
+apt-get install -y kibana 
+cp /vagrant/software/kibana.conf /etc/nginx/sites-available/default
+touch /etc/nginx/htpasswd.users
+if ! grep -q kibanaadmin /etc/nginx/htpasswd.users; then
+    echo "kibanaadmin:$(openssl passwd -apr1 -in /vagrant/.kibana)" >> /etc/nginx/htpasswd.users
+fi
+systemctl restart nginx kibana ##Reinicio necesario para detectar cambios
